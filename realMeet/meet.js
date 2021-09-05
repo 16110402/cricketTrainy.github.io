57ad6a3c8f88da24371dbdc6d3bd25b671de7aa7
Amit yadav <faizabadamit26@gmail.com>
2021-09-05 07:22:20 +0530

    check again

---
+++ realMeet/meet.js
@@ -0,0 +1,14 @@
+function sec1(block,sec){
+     var vill="sheikhpur"//window.prompt("enter your village name here");
+     sec.set(block,vill);
+    //  console.log(sec);
+     return(sec);
+}
+console.log("my name is khan");
+const root=new Map();
+const sec=new Map();
+var dist="faizabad"//window.prompt("enter the district");
+var block="sohawal"//window.prompt("enter the block");
+root.set(dist,block1=sec1(block,sec));
+console.log(root);
+console.log(root.value);
